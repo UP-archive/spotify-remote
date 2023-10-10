120111b3de11315727fa7298067a54283f03d3ba
Suphakit P <me@suphakit.net>
2023-10-10 07:05:21 +0700

    feat<APIs>: split client from main file

--- src/modules/APIs/main.js
+++ src/modules/APIs/main.js
@@ -1,20 +1,11 @@
-const SpotifyWebApi = require('spotify-web-api-node')
-
 const { AuthIn, AuthCallback } = require('./auth')
-
-const sAPI = new SpotifyWebApi({
-  clientId: process.env.clientID,
-  clientSecret: process.env.clientSecret,
-  redirectUri: process.env.redirect_uri,
-})
+const sAPI = require('./client')
 
 async function SpotifyMain(app, root) {
   // Auth
   app.get('/api/auth/login', async (req, res) => {
     await AuthIn(req, res, sAPI)
   })
-
-  app.get('/api/auth/logout', (req, res) => {})
 
   app.get('/api/auth/callback', async (req, res) => {
     await AuthCallback(req, res, sAPI)
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,22 @@
-const url = require('url')
 const SpotifyMain = require('./modules/APIs/main')
+const sAPI = require('./modules/APIs/client')
 
 async function Routes(app, root) {
-  app.get('/', (req, res) => {
-    console.log(req.session.spotifyAccount)
-    res.json(req.session.spotifyAccount)
+  app.get('/', async (req, res) => {
+    // console.log(req.session.spotifyAccount)
+    // res.json(req.session.spotifyAccount)
+    try {
+      sAPI.setAccessToken(req.session.spotifyAccount['access_token'])
+      sAPI.setRefreshToken(req.session.spotifyAccount['refresh_token'])
+      var result = await sAPI.getMe()
+      console.log(result.body)
+      res.status(200).send(result.body)
+    } catch (err) {
+      res.status(400).send(err)
+    }
   })
 
-  SpotifyMain(app, root)
+  await SpotifyMain(app, root)
 }
 
 module.exports = Routes
